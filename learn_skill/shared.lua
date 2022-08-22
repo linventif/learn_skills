176f36c4c324e1afeb8a855f7157ad0ebafef7cd
Linventif <linventif@gmail.com>
2022-08-22 23:44:02 +0200

    2022 08 22

---
+++ learn_skill/shared.lua
@@ -0,0 +1,7 @@
+ENT.Type = "anim";
+ENT.Base = "base_entity"
+ENT.PrintName = "Skill Learn"
+ENT.Author = "Linventif"
+ENT.Category = "Linventif"
+ENT.Spawnable = true
+ENT.Contact = "https://dsc.gg/linventif"
